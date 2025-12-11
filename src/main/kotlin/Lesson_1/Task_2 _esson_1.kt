b5ef9159f521bafe4e3165d1faba6449f44482cb
Grogory <hatebreeders@mail.ru>
2025-12-11 18:37:19 +0700

    T1_L1 create smth

---
+++ src/main/kotlin/Lesson_1/Task_2 _esson_1.kt
@@ -0,0 +1,15 @@
+package org.example.Lesson_1
+
+fun main(){
+    var buyer: Int = 75
+    var gratitude: String = ("Thank you for your order!")
+   // println(gratitude + "Your order was:" + buyer)
+    println(buyer)
+    println(gratitude)
+    var store_Employe: Int = 2000
+    //В компании работает 2000 сотрудников
+    //println("Employees:" + store_Employe)
+    //Один уволился
+    println("Employees:" + (store_Employe - 1))
+
+}
