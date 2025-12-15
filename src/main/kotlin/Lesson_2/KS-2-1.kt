3d6f47a8d24e815e50f62c23dae1dca2ea2e5302
Grogory <hatebreeders@mail.ru>
2025-12-15 21:25:56 +0700

    KS-1-2 Ответ по 1 задаче урока 2

--- src/main/kotlin/Lesson_2/KS-2-1.kt
+++ src/main/kotlin/Lesson_2/KS-2-1.kt
@@ -1,4 +1,13 @@
-fun main() {
-    println(String.format("Средний балл: %.2f", (3 + 4 + 3 + 5) / 4.0))
+const val STUDENT_COUNT = 4
+const val DECIMAL_PLACES = 2
 
+fun main() {
+    val score1 = 3
+    val score2 = 4
+    val score3 = 3
+    val score4 = 5
+
+    val averageScore = (score1 + score2 + score3 + score4).toDouble() / STUDENT_COUNT
+
+    println(String.format("%.${DECIMAL_PLACES}f", averageScore))
 }
