55f546972d0e53e115e539a3afaa3aae2843bd63
Grogory <hatebreeders@mail.ru>
2025-12-15 20:33:03 +0700

    KS-3-1 Ответ по 3 задаче урока 1 Refactor

---
+++ src/main/kotlin/Lesson_1/KS-3-1.kt
@@ -0,0 +1,19 @@
+package org.example.Lesson_1
+
+fun main() {
+    val year = 1961
+    var launchTime = "09:07"
+    var landingTime = "10:55"
+
+    val launchHour = 9
+    val launchMinute = 7
+
+    println(year)
+    println(launchHour)
+    println(launchMinute)
+
+    val landingHour = 10
+    val landingMinute = 55
+
+    println("$landingHour:$landingMinute")
+}
